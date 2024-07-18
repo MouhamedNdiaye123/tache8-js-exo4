52e39d20d121f63734308bc719f48cadaa0f3bec
Mouhamed Ndiaye <mouhamethndiaye123@gmail.com>
2024-07-18 09:47:40 +0000

    ajout des fichiers du tache8 js exo4

---
+++ tache8_4.js
@@ -0,0 +1,9 @@
+// Utilisez la méthode .sort() pour trier un tableau de chaînes par ordre alphabétique.
+
+let trieTab = ['x', 'f', 's', 'e', 'r', 'q', 'o', 'a', 'u', 'c', 'k', 'd', 'z', 'm', 'b', 'p', 'v', 'h',]
+
+alert( "Avant Trie: ---> " + trieTab)
+
+alert("Apres trie: ---> " +trieTab.sort());
+
+
